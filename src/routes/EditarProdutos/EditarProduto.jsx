16abe84eabf6d66ec67cb000ee2c22357cfc72b2
anapaulans <rm552513@fiap.com.br>
2023-09-15 13:54:21 -0300

    configurando o editarProduto

---
+++ src/routes/EditarProdutos/EditarProduto.jsx
@@ -0,0 +1,28 @@
+import {useParams, useNavigate } from "react-router-dom"
+import { ListaProdutos } from "../../components/ListaProdutos"
+
+export default function EditarProduto() {
+
+  const lista = ListaProdutos
+  const navegacao = useNavigate()
+  const {id} = useParams()
+
+  const proc = lista.filter(prod => prod.id == id)
+  const produto = proc[0]
+  
+  const salvar = () => {
+    alert(`Produto: ${produto.nome} editado com sucesso!`)
+    return navegacao('/produtos')
+  }
+  
+  return (
+    <main>
+      <div class="style-product">
+        <h1>PRODUTOS</h1>
+        <h2>{produto.nome}</h2>
+        <h2>R${produto.preco}</h2>
+        <p><img src={`${produto.img}`} alt={`${produto.nome}`} /></p>
+      </div>   
+    </main>
+  )
+}
