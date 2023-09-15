bd81de602c9ab87f58c0608bc58aed4df38f1f4f
CalinaThalya <calina.thalya@icloud.com>
2023-09-15 20:56:32 -0300

    Criação do app css e js.

---
+++ src/listaProduto.js
@@ -0,0 +1,33 @@
+export const listaProdutos = [
+    {
+        id:1,
+        nome: 'iPhone 14 Pro Max',
+        desc: '128G/6.2/Meina Noite',
+        preco: 5700,
+        
+    },
+    {
+        id:2,
+        nome: 'Caixa de som JBL',
+        desc: '24H/Bluetooth/Tam:G',
+        preco: 1200,
+    },
+    {
+        id:3,
+        nome: 'Monitor Sansumg',
+        desc: '32Pol/4K/HD',
+        preco: 1500,
+    },
+    {
+        id:4,
+        nome: 'Playstation 5',
+        desc: 'Console/ 12Jogos/400RAM',
+        preco: 6000,
+    },
+    {
+        id:5,
+        nome: 'Fone Xiaomi',
+        desc: 'Bluetooth/12h ',
+        preco: 950,
+    }
+]
