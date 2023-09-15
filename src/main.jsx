49a5b5aeeeac84c431313a40188eb4cd18c3aa7c
CalinaThalya <calina.thalya@icloud.com>
2023-09-15 20:58:32 -0300

    Criação css + js

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,44 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
 
-import {createBrowserRouter, RouterProvider, Navigate, useNavigate} from 'react-router-dom'
+
 import Home from './routes/Home/Home.jsx'
-import Produtos from './routes/Produtos/Produtos.jsx'
 import Error from './routes/Error/Error.jsx'
-import EditarProduto from './routes/EditarProdutos/EditarProduto.jsx'
+import EditarProdutos from './routes/EditarProdutos/EditarProduto.jsx'
+import Produtos from './routes/Produtos/Produtos.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AdicionarProtudo from './routes/AdicionarProduto/AdicionarProduto.jsx'
 
-const router = createBrowserRouter([{
-  path: '/',
-  element: <App/>,
-  errorElement: <Error/>,
-  children:[
+const route = createBrowserRouter([
     {
-      path:"/",
-      element:<Home/>
-    },
-    {
-      path: "/produtos",
-      element:<Produtos />
-    },
-    {
-      path:"produtos/editar/:id",
-      element: <EditarProduto/>
-    },
-    {
-      path: "/antiga",
-      element: <Navigate to="/"/>
+      path:'/',
+      element: <App/>,
+      errorElement: <Error/>,
+      children:[
+        {
+          path:'/',
+          element: <Home/>
+        },
+        {
+          path:'/produtos',
+          element: <Produtos/>
+        },
+        {
+          path:'/editar/produtos/:id',
+          element: <EditarProdutos/>
+        },
+        {
+          path:'/adicionar/produtos',
+          element: <AdicionarProtudo/>
+        }
+      ]
+
     }
-  ]
-}])
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router = {route}/>
   </React.StrictMode>,
 )
